10aacbebd5d5e1b616042c05e76f38e73df494f3
ustrungnguyen <contact@ngtrung.com>
2025-09-03 09:45:08 +0700

    Updated About Us

--- src/pages/Introduction/sub-components/DeploymentTimeline.js
+++ src/pages/Introduction/sub-components/DeploymentTimeline.js
@@ -1,38 +1,19 @@
 import timelineData from '../data/timeline-data.json';
 
-const TimelineYearPlacement = () => {
-    timelineData.map((data) => {
-        if(data.id % 2 !== 0) {
-        return (
-            <div className='timeline-year-container'>
-                <h4 className='timeline-title'>{data.title}</h4>
-                <span className='timeline-year'>{data.year}</span>
-            </div>
-        )
-        } else {
-            return (
+const TimelineItem = ({data}) => {
+    return (
+        <div className='timeline-item'>
+            <div className='timeline-dot'></div>
+            <div className='timeline-content'>
                 <div className='timeline-year-container'>
                     <span className='timeline-year'>{data.year}</span>
                     <h4 className='timeline-title'>{data.title}</h4>
                 </div>
-            )
-        }
-    })
-};
-
-const TimelineItem = ({data}) => (
-    <div className='timeline-item'>
-        <div className='timeline-dot'></div>
-        <div className='timeline-content'>
-            {/* <div className='timeline-year-container'>
-                <span className='timeline-year'>{data.year}</span>
-                <h4 className='timeline-title'>{data.title}</h4>
-            </div> */}
-            <TimelineYearPlacement />
-            <p className='content-description'>{data.description}</p>
+                <p className='content-description'>{data.description}</p>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default function DeploymentTimeline() {
     return (
