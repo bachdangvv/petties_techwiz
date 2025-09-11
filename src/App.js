1ee50b01c98eed8f96b4b247c9286b291d9484d4
bachdangvv <bachdangvv@gmail.com>
2025-09-11 16:24:40 +0700

    sua app js blog

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
               <Route path="/about" element={<Introduction />} />
               <Route path="/cart" element={<Cart />}/>
               <Route path="/product/:id" element={<ProductDetail />} />
-              <Route path="/blog" element={<Blog />}/>
+              <Route path="/blog" element={<div className="blog-page"><Blog /></div>}/>
 
               {/* Testing Area */}
               <Route path="/testpage" element={<TestProductPage />} />
